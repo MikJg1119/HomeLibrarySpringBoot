e5707db1fa190f407775f076d5a217f5c4b5621d
Mikołaj J <mikigraf@wp.pl>
2021-09-22 18:12:01 +0200

    Internationalization for Polish

--- src/main/java/com/example/HomeLibrarySpringBoot/config/Internationalization.java
+++ src/main/java/com/example/HomeLibrarySpringBoot/config/Internationalization.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -17,9 +17,9 @@ public class Internationalization implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver(){
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
-        return sessionLocaleResolver;
+        CookieLocaleResolver cookieLocaleResolver=new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return cookieLocaleResolver;
     }
 
     @Bean
