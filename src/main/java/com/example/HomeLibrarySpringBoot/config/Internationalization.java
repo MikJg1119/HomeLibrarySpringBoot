b6c024e4d86395718d1d69440bd2aaa952718630
Mikołaj J <mikigraf@wp.pl>
2021-09-22 17:49:00 +0200

    Internationalization for Polish

--- src/main/java/com/example/HomeLibrarySpringBoot/config/Internationalization.java
+++ src/main/java/com/example/HomeLibrarySpringBoot/config/Internationalization.java
@@ -1,6 +1,7 @@
 package com.example.HomeLibrarySpringBoot.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @Configuration
+@ComponentScan(basePackages = "com.example.HomeLibrarySpringBoot.config")
 public class Internationalization implements WebMvcConfigurer {
 
     @Bean
