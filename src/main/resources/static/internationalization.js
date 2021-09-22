ac326fa0cb8826a2999ef3de737c219e993bf182
Mikołaj J <mikigraf@wp.pl>
2021-09-22 17:40:11 +0200

    Internationalization for Polish

--- src/main/resources/static/internationalization.js
+++ src/main/resources/static/internationalization.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#locales").change(function () {
         var selectedOption = $('#locales').val();
         if (selectedOption != ''){
-            window.location.replace('international?lan=' + selectedOption);
+            window.location.replace('?lan=' + selectedOption);
         }
     });
 });
