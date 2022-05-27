868943ece9dead26740d6a1dbfe6c81a6c1b8bb0
Mikołaj J <mikigraf@wp.pl>
2022-05-27 19:09:15 +0200

    BookController fix

--- src/main/java/com/example/HomeLibrarySpringBoot/controller/BookController.java
+++ src/main/java/com/example/HomeLibrarySpringBoot/controller/BookController.java
@@ -9,8 +9,6 @@ import com.example.HomeLibrarySpringBoot.service.UsersLibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +18,8 @@ import java.util.List;
 @RestController
 public class BookController {
 
-//    @Autowired
     private BookService bookService;
-
-//    @Autowired
     private UserService userService;
-
-//    @Autowired
     private UsersLibraryService usersLibraryService;
 
 
@@ -66,10 +59,7 @@ public class BookController {
 
             return HttpStatus.NOT_FOUND;
         }
-        User user = userService.getUserByEmail(email);
-        if (email == null){
-            user = userService.getUserById(userId);
-        }
+        User user = email !=null ? userService.getUserByEmail(email) : userService.getUserById(userId);
         UsersLibrary usersLibrary = usersLibraryService.getUsersLibraryByUser(user);
        usersLibrary.getBooks().add(book);
         usersLibraryService.save(usersLibrary);
@@ -77,34 +67,25 @@ public class BookController {
     }
 
     @PostMapping("/updateBook")
-    public HttpStatus updateBook(@RequestBody Book book){
+    public HttpStatus updateBook(@RequestBody Book book,
+                                 @RequestParam @Nullable String email,
+                                 @RequestParam @Nullable int userId){
         bookService.updateBook(book);
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        User user = userService.getUserByEmail(username);
+        User user = email !=null ? userService.getUserByEmail(email) : userService.getUserById(userId);
         UsersLibrary usersLibrary = usersLibraryService.getUsersLibraryByUser(user);
         usersLibrary.getBooks().add(book.getId(),book);
         return HttpStatus.ACCEPTED;
     }
 
-//    @GetMapping("/showFormForUpdate/{id}")
-//    public HttpStatus showFormForUpdate(@PathVariable(value = "id") int id, Model model){
-//        Book book = bookService.getBook(id);
-//        model.addAttribute("book", book);
-//        return HttpStatus.ACCEPTED;
-//
-//    }
 
     @DeleteMapping("/deleteBook/{id}")
     public HttpStatus deleteBook(@PathVariable(value = "id") int id,
-                                    @RequestParam int userId){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        User user = userService.getUserByEmail(username);
+                                 @RequestParam @Nullable String email,
+                                 @RequestParam @Nullable int userId){
+        User user = email !=null ? userService.getUserByEmail(email) : userService.getUserById(userId);
         UsersLibrary usersLibrary = usersLibraryService.getUsersLibraryByUser(user);
         usersLibrary.getBooks().remove(bookService.getBook(id));
         usersLibraryService.save(usersLibrary);
-
 
         return HttpStatus.ACCEPTED;
     }
