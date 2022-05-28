c64f6720d76cb3b53d630fafdef26826c9c7f36a
Mikołaj J <mikigraf@wp.pl>
2022-05-28 13:53:23 +0200

    Depreciating MainController and update of UserRegistrationController

--- src/main/java/com/example/HomeLibrarySpringBoot/controller/MainController.java
+++ src/main/java/com/example/HomeLibrarySpringBoot/controller/MainController.java
@@ -1,21 +1,20 @@
 package com.example.HomeLibrarySpringBoot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
-@RestController
+@Controller
+@Deprecated
 public class MainController {
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String loggingIn(){
-        return "redirect:/";
-    }
+//    @GetMapping("/login")
+//    public String login(){
+//        return "login";
+//    }
+//
+//    @PostMapping("/login")
+//    public String loggingIn(){
+//        return "redirect:/";
+//    }
 }
--- src/main/java/com/example/HomeLibrarySpringBoot/controller/UserRegistrationController.java
+++ src/main/java/com/example/HomeLibrarySpringBoot/controller/UserRegistrationController.java
@@ -5,6 +5,7 @@ import com.example.HomeLibrarySpringBoot.model.User;
 import com.example.HomeLibrarySpringBoot.service.UserService;
 import com.example.HomeLibrarySpringBoot.service.UsersLibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -22,15 +23,18 @@ public class UserRegistrationController {
         return new UserRegistrationDto();
     }
 
-    @GetMapping
-    public String showRegistrationForm() {
-        return "registration";
-    }
+//    @GetMapping
+//    public String showRegistrationForm() {
+//        return "registration";
+//    }
 
     @PostMapping
-    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+    public HttpStatus registerUserAccount(@RequestBody UserRegistrationDto registrationDto) {
         User user = userService.save(registrationDto);
-        return "redirect:/registration?success";
+        if (user ==null){
+            return HttpStatus.BAD_REQUEST;
+        }
+        return HttpStatus.ACCEPTED;
     }
 
 }
