aa9e3b5bbbe1212f1fd0125eb1c0344b966c333f
Mikołaj J <mikigraf@wp.pl>
2022-11-16 20:12:20 +0100

    switching to DTO in controllers for clarity in communication (preparation to save images and files)

--- src/main/java/com/example/HomeLibrarySpringBoot/model/dto/AuthorDto.java
+++ src/main/java/com/example/HomeLibrarySpringBoot/model/dto/AuthorDto.java
@@ -1,4 +1,13 @@
 package com.example.HomeLibrarySpringBoot.model.dto;
 
-public class AuthorDto {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class AuthorDto implements Serializable {
+    private String name;
+    private int id;
 }
